a0eca4f01373e68462c9ab7de3cb2b21c6cf2b68
paulkani <paulkani.s@golden-hippo.com>
2023-07-28 21:13:34 +0530

    uppercase

---
+++ main.js
@@ -0,0 +1,10 @@
+var a=prompt("Enter the word");
+document.write("The given word is "+a+"<br>"+"<br>");
+var c=a.replace(a[0],a[0].toUpperCase());
+document.write("To Capitalized  ")
+document.write(c);
+
+
+
+
+
